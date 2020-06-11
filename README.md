57cc8a611992fc0552cc0caf39f697a423771fec
nitinpunn <36107629+nitinpunn@users.noreply.github.com>
2020-06-11 07:56:15 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# prnt.sc Scraper
+
+This is a Python script to scrape images from prnt.sc
+
+## Background
+
+Prnt.sc uses a simple 6 character alphanumeric sequential code for each image. Therefore it is extremely easy to scrape for images.
+
+## Usage
+
+The prnt.py file and the pictures folder should be in the same directory and pictures directory must exist.
+Mechanize and BeautifulSoup are needed for this and can be installed by the following commands:
+```python
+pip3 install beautifulsoup4
+pip3 install mechanize 
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ prnt.py
@@ -0,0 +1,47 @@
+import random
+import string
+import mechanize
+import bs4
+
+# The random combinations will be stored here
+combos = []
+
+# Create a browser object form mechanize and add a realistic header to prevent a 403
+browser = mechanize.Browser()
+browser.set_handle_robots(False)
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+# Fixes an issue that sometimes may come up where https: is missing
+def fixSlash(img_url):
+    if img_url[0] == "/":
+        return "https:" + img_url
+    else:
+        return img_url
+
+# Generates the 6 character alphanumeric combinations and adds them to the combos list
+def generateRandom(amount):
+    print("Generating random combinations")
+    for i in range(amount):
+        combos.append(''.join(random.choices(string.ascii_lowercase + string.digits, k=6)))
+
+# Takes the combos list and sequentially loads each link and downloads the image
+def tryRandom(tries):
+    print("Grabbing images")
+    samples = random.sample(combos, tries)
+    for i in range(len(samples)):
+        url = "https://prnt.sc/" + samples[i]
+        browser.open(url)
+        html = browser.response().read()
+        temp = bs4.BeautifulSoup(html, features="html5lib")
+        images = [img['src'] for img in temp.findAll('img') if img.has_attr('src')]
+        image = images[0]
+        image = fixSlash(image)
+        picture = browser.retrieve(image, "pictures/" + samples[i] + ".jpg")
+        print("Done with " + str(i+1) + "/" + str(amount))
+
+if __name__ == "__main__":
+    amount = int(input("Enter amount of images to scrape: "))
+    generateRandom(amount)
+    print("Combinations generated!")
+    tryRandom(amount)
+    print("Done!")
